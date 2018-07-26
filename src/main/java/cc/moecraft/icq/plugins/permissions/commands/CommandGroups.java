4d90ae64fe189a4ca58c4162075568e4a15d27bc
Hykilpikonna <admin@moecraft.cc>
2018-07-26 20:48:04 +0800

    [+] grp 权限组管理指令

---
+++ src/main/java/cc/moecraft/icq/plugins/permissions/commands/CommandGroups.java
@@ -0,0 +1,58 @@
+package cc.moecraft.icq.plugins.permissions.commands;
+
+import cc.moecraft.icq.command.CommandProperties;
+import cc.moecraft.icq.command.interfaces.EverywhereCommand;
+import cc.moecraft.icq.event.events.message.EventMessage;
+import cc.moecraft.icq.plugins.permissions.*;
+import cc.moecraft.icq.user.User;
+
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/26 创建!
+ * Created by Hykilpikonna on 2018/07/26!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class CommandGroups implements EverywhereCommand
+{
+    /**
+     * 用法:
+     *  管理权限组:
+     *
+     *  !grp list                          查看权限组列表
+     *  !grp info [名字]                   查询权限组 2
+     *  !grp create [名字]                 创建权限组 2
+     *  !grp remove [名字]                 移除权限组 2
+     *  !grp perm add [名字] [权限]        添加权限   4
+     *  !grp perm remove [名字] [权限]     移除权限   4
+     *  !grp group add [名字] [权限组]     添加继承   4
+     *  !grp group remove [名字] [权限组]  移除继承   4
+     *
+     * @param event 消息事件
+     * @param user 发送者
+     * @param command 指令
+     * @param args 参数
+     * @return 回复
+     */
+    @Override
+    public String run(EventMessage event, User user, String command, ArrayList<String> args)
+    {
+        if (!Permissions.verifyAndSendText(event, user.getId(), "permissionmanager.groups")) return null;
+        return help(command);
+    }
+
+    @Override
+    public CommandProperties properties()
+    {
+        return new CommandProperties("permissiongroups", "groups", "grp");
+    }
+
+    private String help(String command)
+    {
+        return String.format("!%s [info|create|remove] <权限组名>", command) + "\n" +
+                String.format("!%s [perm|group] [add|remove] <权限组名> [权限|子权限组]", command);
+    }
+}
