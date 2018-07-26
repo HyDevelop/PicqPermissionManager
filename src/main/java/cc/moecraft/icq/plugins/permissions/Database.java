10c486aa8af6d1b01d5e41f30ae250de3b33e64d
Hykilpikonna <admin@moecraft.cc>
2018-07-26 20:37:29 +0800

    [+] 权限组数据库类

---
+++ src/main/java/cc/moecraft/icq/plugins/permissions/Database.java
@@ -0,0 +1,37 @@
+package cc.moecraft.icq.plugins.permissions;
+
+import cc.moecraft.yaml.Config;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/17 创建!
+ * Created by Hykilpikonna on 2018/07/17!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class Database extends Config
+{
+
+    public Database()
+    {
+        super("data", "permissions", "yml", false, true, true);
+    }
+
+    /**
+     * 预加载, 可以重载
+     */
+    @Override
+    public void readConfig()
+    {
+    }
+
+    @Override
+    public void writeDefaultConfig()
+    {
+    }
+}
