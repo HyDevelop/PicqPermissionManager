1743b7fb41fdf4ce75f9cae314915c824612d89f
Hykilpikonna <admin@moecraft.cc>
2018-07-26 20:43:32 +0800

    [+] static工具类

---
+++ src/main/java/cc/moecraft/icq/plugins/permissions/Permissions.java
@@ -0,0 +1,16 @@
+package cc.moecraft.icq.plugins.permissions;
+
+import cc.moecraft.icq.PicqBotX;
+import cc.moecraft.icq.event.events.message.EventMessage;
+
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/22 创建!
+ * Created by Hykilpikonna on 2018/04/22!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ */
+public class Permissions
+{
+}
